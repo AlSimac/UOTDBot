7d26b360448bc179e91f05466e3319ecea03f2c2
AlSimac <alexis.simac@yahoo.fr>
2024-01-04 18:49:46 +0100

    add map check logic

--- UOTDBot/Scheduler.cs
+++ UOTDBot/Scheduler.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using GBX.NET.Engines.Game;
+using GBX.NET;
+using GBX.NET.LZO;
 
 namespace UOTDBot;
 public interface IScheduler
@@ -32,6 +35,121 @@ internal sealed class Scheduler : BackgroundService, IScheduler
     {
         // ... something every second ...
 
+        Console.WriteLine("< Start Program.Main");
+
+        // 02/01/2024 before NadeoAPI is set up
+        String filepath = "C:\\Users\\STRpo\\Documents\\coding\\MapParsing\\maps\\cotd.Map.Gbx";
+
+        // 02/09/2020 loading one file
+        GameBox<CGameCtnChallenge> gbx = LoadGBX(filepath);
+        List<String> resultList = getAllCars(gbx);
+
+        // 01/01/2024 Do something only if the list isn't empty
+        if (resultList != null)
+        {
+
+            // 01/01/2024 maybe add a car emote idk
+            foreach (String result in resultList)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        else
+        {
+            Console.WriteLine("nothing");
+        }
+
+        Console.WriteLine("> End Program.Main");
+
         return Task.CompletedTask;
+    }
+
+    // 02/09/2020 Load the GBX
+    public GameBox<CGameCtnChallenge> LoadGBX(string mapPath)
+    {
+        Console.WriteLine("< Start ReaderProgram.LoadGBX");
+        //string mapPath = "C:\\Users\\STRpo\\Documents\\Ultimate Nadeo Maps\\UpdatedFiles\\TrackMania\\1.2.3\\PuzzleD3.Challenge.gbx";
+
+        // 31/08/2020 Reading GBX
+        var gbx = GameBox.Parse<CGameCtnChallenge>(mapPath);
+
+        Console.WriteLine("> End ReaderProgram.LoadGBX");
+        return gbx;
+    }
+
+    // 01/01/2024 Main method to check the default car and all car change gates
+    public List<String> getAllCars(CGameCtnChallenge map)
+    {
+        Console.WriteLine("< Start ReaderProgram.getAllCars");
+
+        Console.WriteLine("[-- Writing block info for {0} --]", map.MapName);
+
+        List<String> carList = new List<String>();
+        bool alert = false;
+        string defaultCar = map.PlayerModel.Id;
+
+        // 01/01/2024 if PlayerModel is empty, it's CarSport
+        if (defaultCar == "")
+        {
+            defaultCar = "CarSport";
+        }
+
+        carList.Add(defaultCar);
+
+        // 01/01/2024 if CarSport, check if there are car change Gates
+        if (defaultCar == "CarSport" || defaultCar == null)
+        {
+            IList<CGameCtnBlock> blocks = map.Blocks;
+            IList<CGameCtnAnchoredObject> items = map.AnchoredObjects;
+
+            // 01/01/2024 loop on blocks
+            foreach (CGameCtnBlock block in blocks)
+            {
+                // 01/01/2024 Need to find a better way to get all car change blocks
+                if (block.Name.Contains("GameplaySnow"))
+                {
+                    Console.WriteLine("Found a car change gate block");
+                    carList.Add("CarSnow");
+                    alert = true;
+                }
+            }
+
+            foreach (CGameCtnAnchoredObject item in items)
+            {
+
+                // 01/01/2024 and also a better way to list the cars
+                if (item.ItemModel.Id.Contains("GameplaySnow"))
+                {
+                    Console.WriteLine("Found a car change gate item");
+                    carList.Add("CarSnow");
+                    alert = true;
+                }
+            }
+        }
+
+        // 01/01/2024 if default car is anything else, return true
+        else
+        {
+            Console.WriteLine("Default car: " + defaultCar);
+            alert = true;
+        }
+
+        // 01/01/2024 if there's something else than CarSport, return the list
+        if (alert == true)
+        {
+            carList = carList.Distinct().ToList();
+            Console.WriteLine("> End ReaderProgram.getAllCars");
+            return carList;
+        }
+
+        // 01/01/2024 otherwise return null
+        else
+        {
+            Console.WriteLine("> End ReaderProgram.getAllCars");
+            return null;
+        }
+
+
     }
 }
