dabf608c788316da925c7e417c990ec8987e6bda
Petr Pivoňka <petrpiv1@gmail.com>
2024-01-22 00:40:17 +0100

    Merge App into Program

--- UOTDBot/Program.cs
+++ UOTDBot/Program.cs
@@ -1,11 +1,51 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using Discord;
+using ManiaAPI.NadeoAPI;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using UOTDBot;
 
-var services = new ServiceCollection();
+var builder = Host.CreateDefaultBuilder(args);
 
-await Host.CreateDefaultBuilder(args)
-    .ConfigureServices((context, services) => App.Services(services))
-    .UseApp()
-    .Build()
-    .RunAsync();
+builder.ConfigureServices((context, services) =>
+{
+    // Configure Discord bot
+    services.AddSingleton(new DiscordSocketConfig()
+    {
+        LogLevel = LogSeverity.Verbose
+    });
+
+    // Add Discord bot client and Interaction Framework
+    services.AddSingleton<DiscordSocketClient>();
+    services.AddSingleton<InteractionService>(provider => new(provider.GetRequiredService<DiscordSocketClient>(), new()
+    {
+        LogLevel = LogSeverity.Verbose
+    }));
+
+    // Add Serilog
+    services.AddLogging(builder =>
+    {
+        builder.AddSerilog(dispose: true);
+    });
+
+    // Add startup
+    services.AddHostedService<Startup>();
+    services.AddHostedService<Scheduler>();
+
+    // Add services
+    services.AddSingleton<IDiscordBot, DiscordBot>();
+
+    // 01/01/2024 Add ManiaAPI.NadeoAPI
+    services.AddSingleton<NadeoServices>();
+});
+
+// Use Serilog
+builder.UseSerilog();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(outputTemplate: "[{SourceContext} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .CreateLogger();
+
+await builder.Build().RunAsync();
