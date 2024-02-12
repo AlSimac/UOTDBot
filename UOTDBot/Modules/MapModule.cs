d5fe6b6d4d9db0093f3c841568d4463ef03db41c
Petr Pivoňka <petrpiv1@gmail.com>
2024-02-12 10:01:09 +0100

    Add MapModule

---
+++ UOTDBot/Modules/MapModule.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Interactions;
+
+namespace UOTDBot.Modules;
+
+[Group("map", "UOTD maps.")]
+public sealed class MapModule : InteractionModuleBase<SocketInteractionContext>
+{
+    private readonly AppDbContext _db;
+
+    public MapModule(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    [SlashCommand("list", "Get the UOTD map list.")]
+    public async Task List()
+    {
+        
+    }
+}
