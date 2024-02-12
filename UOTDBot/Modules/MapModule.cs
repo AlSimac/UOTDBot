d6f5826c0184f1ec9c5c2f0c7176234c1ed5cca7
Petr Pivoňka <petrpiv1@gmail.com>
2024-02-12 12:33:47 +0100

    Add map list

--- UOTDBot/Modules/MapModule.cs
+++ UOTDBot/Modules/MapModule.cs
@@ -1,4 +1,7 @@
-﻿using Discord.Interactions;
+﻿using Discord;
+using Discord.Interactions;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
 
 namespace UOTDBot.Modules;
 
@@ -15,6 +18,21 @@ public sealed class MapModule : InteractionModuleBase<SocketInteractionContext>
     [SlashCommand("list", "Get the UOTD map list.")]
     public async Task List()
     {
-        
+        var maps = await _db.Maps
+            .OrderByDescending(x => x.Totd)
+            .Take(10)
+            .ToListAsync();
+
+        var sb = new StringBuilder();
+
+        foreach (var map in maps)
+        {
+            sb.AppendLine($"**{map.Totd:yyyy-MM-dd}** - {map.Name} by {map.AuthorName}");
+        }
+
+        await RespondAsync(embed: new EmbedBuilder()
+            .WithTitle("UOTD maps")
+            .WithDescription(sb.ToString())
+            .Build(), ephemeral: true);
     }
 }
